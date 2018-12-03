a198d9ae4fdd9acac2da37a4278b075a652daa37
Alex Dukhno <Alex-Diez@users.noreply.github.com>
2018-12-03 09:19:50 +0200

    todo list kata tenth day

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TodoList from './todo-list-kata/day_09/kata';
+import TodoList from './todo-list-kata/day_10/kata';
 
 class App extends Component {
   render() {
