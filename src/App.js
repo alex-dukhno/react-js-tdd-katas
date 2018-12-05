ae54c2d8dcee3e3664c0edc90e406e913164f62c
Alex-Diez <alex.dukhno@icloud.com>
2018-12-05 20:11:53 +0200

    todo list kata thirteenth day

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TodoList from './todo-list-kata/day_12/kata';
+import TodoList from './todo-list-kata/day_13/kata';
 
 class App extends Component {
   render() {
